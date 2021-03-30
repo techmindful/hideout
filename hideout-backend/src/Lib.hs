b3c95173d5018ba65b7704dc797778ab737d9078
Tech Mindful <hello@techmindful.blog>
2021-03-30 02:48:44 -0700

    Backend's barebone read-letter and write-letter handlers.

--- hideout-backend/src/Lib.hs
+++ hideout-backend/src/Lib.hs
@@ -9,30 +9,41 @@ module Lib
     ) where
     
 import qualified Servant
-import           Servant ( (:>), Capture, Get )
+import           Servant ( (:>), (:<|>)(..), Capture, Get, NoContent(..), Put, ReqBody )
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 
 import           Data.Aeson ( FromJSON, ToJSON )
 
+import           Control.Monad.IO.Class
 import           GHC.Generics ( Generic )
 
 
-data Letter = Letter String
+data Letter = Letter
+  { body :: String }
   deriving ( Generic )
 instance FromJSON Letter
 instance ToJSON Letter
 
 
-type API = "read-letter" :> Capture "letterId" String :> Get '[ Servant.JSON ] Letter
+type API = "read-letter"  :> Capture "letterId" String :> Get '[ Servant.JSON ] Letter
+      :<|> "write-letter" :> ReqBody '[ Servant.JSON ] Letter :> Put '[ Servant.JSON ] NoContent
 
 
-testLetter :: String -> Servant.Handler Letter
-testLetter letterId = return (Letter "test")
+readLetter :: String -> Servant.Handler Letter
+readLetter letterId = do
+  return (Letter "test")
+
+
+writeLetter :: Letter -> Servant.Handler NoContent
+writeLetter letter = do
+  liftIO $ putStrLn "write"
+  return NoContent
 
 
 server :: Servant.Server API
-server = testLetter
+server = readLetter
+    :<|> writeLetter
 
 
 api :: Servant.Proxy API
