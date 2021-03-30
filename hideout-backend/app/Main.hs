5eab39d8bdedf6b98844df2598440e24b51e3908
Tech Mindful <hello@techmindful.blog>
2021-03-30 00:22:13 -0700

    API takes a String.

--- hideout-backend/app/Main.hs
+++ hideout-backend/app/Main.hs
@@ -1,18 +1,6 @@
-{-# language DataKinds #-}
-{-# language TypeOperators #-}
-
 module Main where
 
-import qualified Data.Aeson
-import qualified Servant
-import           Servant ( (:>), Capture, Get )
-
-
-type API = "read-letter" :> Capture "letterId" String :> Get '[Servant.JSON] Letter
-
-
-type Letter = String
-
+import Lib
 
 main :: IO ()
-main = putStrLn "hi"
+main = startApp
--- hideout-backend/src/Lib.hs
+++ hideout-backend/src/Lib.hs
@@ -1,40 +1,47 @@
 {-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+
 module Lib
     ( startApp
     , app
     ) where
+    
+import qualified Servant
+import           Servant ( (:>), Capture, Get )
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
+import           Data.Aeson ( FromJSON, ToJSON )
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+import           GHC.Generics ( Generic )
 
-$(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+data Letter = Letter String
+  deriving ( Generic )
+instance FromJSON Letter
+instance ToJSON Letter
+
+
+type API = "read-letter" :> Capture "letterId" String :> Get '[ Servant.JSON ] Letter
+
+
+testLetter :: String -> Servant.Handler Letter
+testLetter letterId = return (Letter "test")
+
+
+server :: Servant.Server API
+server = testLetter
+
+
+api :: Servant.Proxy API
+api = Servant.Proxy
+
+
+app :: Servant.Application
+app = Servant.serve api server
+
 
 startApp :: IO ()
-startApp = run 8080 app
-
-app :: Application
-app = serve api server
-
-api :: Proxy API
-api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+startApp = Warp.run 8080 app
