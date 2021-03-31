9239c6c4d1fa778592dba1925cb4a4b50a37434a
Tech Mindful <hello@techmindful.blog>
2021-03-30 23:34:22 -0700

    Server now has an AppState. It can insert new letter into a map in the AppState. Next is generating random letter ID.

--- hideout-backend/src/Lib.hs
+++ hideout-backend/src/Lib.hs
@@ -18,12 +18,18 @@ import           Network.Wai.Middleware.Cors ( cors, CorsResourcePolicy(..) )
 import           Data.Aeson ( FromJSON, ToJSON )
 
 import           Control.Monad.IO.Class
+import           Control.Monad.Reader ( ReaderT, ask, runReaderT )
+import           Control.Monad.STM ( atomically )
+import           Control.Concurrent.STM.TVar ( TVar, newTVar, readTVar, writeTVar )
+import           Data.Function ( (&) )
+import qualified Data.Map.Strict as Map
+import           Data.Map.Strict ( Map(..) )
 import           GHC.Generics ( Generic )
 
 
 data Letter = Letter
   { body :: String }
-  deriving ( Generic )
+  deriving ( Generic, Show )
 instance FromJSON Letter
 instance ToJSON Letter
 
@@ -32,18 +38,33 @@ type API = "read-letter"  :> Capture "letterId" String :> Get '[ Servant.JSON ] 
       :<|> "write-letter" :> ReqBody '[ Servant.JSON ] Letter :> Put '[ Servant.JSON ] NoContent
 
 
-readLetter :: String -> Servant.Handler Letter
+data AppState = AppState
+  { letters :: TVar ( Map String Letter ) }
+
+
+readLetter :: String -> ReaderT AppState Servant.Handler Letter
 readLetter letterId = do
   return (Letter "test")
 
 
-writeLetter :: Letter -> Servant.Handler NoContent
+writeLetter :: Letter -> ReaderT AppState Servant.Handler NoContent
 writeLetter letter = do
+
   liftIO $ putStrLn "write"
+
+  appState <- ask
+
+  liftIO $ do
+    oldLetters <- atomically $ readTVar ( appState & letters )
+    let newLetters = Map.insert "test_id" letter oldLetters
+    atomically $ writeTVar ( appState & letters ) newLetters
+    letters <- atomically $ readTVar ( appState & letters )
+    putStrLn $ show letters
+
   return NoContent
 
 
-server :: Servant.Server API
+server :: Servant.ServerT API (ReaderT AppState Servant.Handler)
 server = readLetter
     :<|> writeLetter
 
@@ -52,8 +73,8 @@ api :: Servant.Proxy API
 api = Servant.Proxy
 
 
-app :: Servant.Application
-app =
+app :: AppState -> Servant.Application
+app appState =
   cors ( \_ -> Just $ CorsResourcePolicy
           { corsOrigins = Nothing
           , corsMethods = [ "GET", "PUT" ]
@@ -65,9 +86,12 @@ app =
           , corsIgnoreFailures = False
           }
        )
-   ( Servant.serve api server )
+  ( Servant.serve api $ Servant.hoistServer api ( \x -> runReaderT x appState ) server )
 
 
 
 startApp :: IO ()
-startApp = Warp.run 8080 app
+startApp = do
+  initLetters <- atomically $ newTVar Map.empty
+  let initAppState = AppState { letters = initLetters }
+  Warp.run 8080 $ app initAppState
